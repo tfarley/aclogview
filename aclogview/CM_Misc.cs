61311e12161e8f78f11bbc3f42229fb93b11dead
tfarley <tfarleygithub@gmail.com>
2017-01-21 19:35:46 -0800

    Add misc packets

--- aclogview/CM_Misc.cs
+++ aclogview/CM_Misc.cs
@@ -13,7 +13,22 @@ public class CM_Misc : MessageProcessor {
 
         PacketOpcode opcode = Util.readOpcode(messageDataReader);
         switch (opcode) {
-
+            case PacketOpcode.Evt_Misc__PortalStorm_ID:
+            case PacketOpcode.Evt_Misc__PortalStormSubsided_ID: {
+                    EmptyMessage message = new EmptyMessage(opcode);
+                    message.contributeToTreeView(outputTreeView);
+                    break;
+                }
+            case PacketOpcode.Evt_Misc__PortalStormBrewing_ID: {
+                    PortalStormBrewing message = PortalStormBrewing.read(messageDataReader);
+                    message.contributeToTreeView(outputTreeView);
+                    break;
+                }
+            case PacketOpcode.Evt_Misc__PortalStormImminent_ID: {
+                    PortalStormImminent message = PortalStormImminent.read(messageDataReader);
+                    message.contributeToTreeView(outputTreeView);
+                    break;
+                }
             default: {
                     handled = false;
                     break;
@@ -23,5 +38,37 @@ public class CM_Misc : MessageProcessor {
         return handled;
     }
 
+    public class PortalStormBrewing : Message {
+        public float extent;
 
+        public static PortalStormBrewing read(BinaryReader binaryReader) {
+            PortalStormBrewing newObj = new PortalStormBrewing();
+            newObj.extent = binaryReader.ReadSingle();
+            return newObj;
+        }
+
+        public override void contributeToTreeView(TreeView treeView) {
+            TreeNode rootNode = new TreeNode(this.GetType().Name);
+            rootNode.Expand();
+            rootNode.Nodes.Add("extent = " + extent);
+            treeView.Nodes.Add(rootNode);
+        }
+    }
+
+    public class PortalStormImminent : Message {
+        public float extent;
+
+        public static PortalStormImminent read(BinaryReader binaryReader) {
+            PortalStormImminent newObj = new PortalStormImminent();
+            newObj.extent = binaryReader.ReadSingle();
+            return newObj;
+        }
+
+        public override void contributeToTreeView(TreeView treeView) {
+            TreeNode rootNode = new TreeNode(this.GetType().Name);
+            rootNode.Expand();
+            rootNode.Nodes.Add("extent = " + extent);
+            treeView.Nodes.Add(rootNode);
+        }
+    }
 }
