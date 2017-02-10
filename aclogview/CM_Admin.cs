4f376715cace2fa1c642a2066b4e1ff651803500
cmoski <reese@allwiretech.com>
2017-02-09 23:09:58 -0800

    A few things

--- aclogview/CM_Admin.cs
+++ aclogview/CM_Admin.cs
@@ -6,15 +6,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-public class CM_Admin : MessageProcessor {
+public class CM_Admin : MessageProcessor
+{
 
-    public override bool acceptMessageData(BinaryReader messageDataReader, TreeView outputTreeView) {
+    public override bool acceptMessageData(BinaryReader messageDataReader, TreeView outputTreeView)
+    {
         bool handled = true;
 
         PacketOpcode opcode = Util.readOpcode(messageDataReader);
-        switch (opcode) {
-
-            default: {
+        switch (opcode)
+        {
+            case PacketOpcode.Evt_Admin__ChatServerData_ID:
+                {
+                    TurbineChat message = TurbineChat.read(messageDataReader);
+                    message.contributeToTreeView(outputTreeView);
+                    break;
+                }
+            default:
+                {
                     handled = false;
                     break;
                 }
@@ -23,5 +32,52 @@ public class CM_Admin : MessageProcessor {
         return handled;
     }
 
+    public class TurbineChat : Message
+    {
+        public uint size;
+        public uint TurbineChatType;
+        public uint Unk1;
+        public uint Unk2;
+        public uint Unk3;
+        public uint Unk4;
+        public uint Unk5;
+        public uint Unk6;
+        public uint payload_size;
 
+
+        public static TurbineChat read(BinaryReader binaryReader)
+        {
+            TurbineChat newObj = new TurbineChat();
+            newObj.size = binaryReader.ReadUInt32();
+            newObj.TurbineChatType = binaryReader.ReadUInt32();
+            newObj.Unk1 = binaryReader.ReadUInt32();
+            newObj.Unk2 = binaryReader.ReadUInt32();
+            newObj.Unk3 = binaryReader.ReadUInt32();
+            newObj.Unk4 = binaryReader.ReadUInt32();
+            newObj.Unk5 = binaryReader.ReadUInt32();
+            newObj.Unk6 = binaryReader.ReadUInt32();
+            newObj.payload_size = binaryReader.ReadUInt32();
+
+            return newObj;
+        }
+
+        public override void contributeToTreeView(TreeView treeView)
+        {
+            TreeNode rootNode = new TreeNode(this.GetType().Name);
+            rootNode.Expand();
+            foreach (var prop in this.GetType().GetProperties())
+            {
+                rootNode.Nodes.Add("prop.Name = " + prop.GetValue(this).ToString());
+            }
+            rootNode.Nodes.Add("size = " + size.ToString("X"));
+            rootNode.Nodes.Add("TurbineChatType = " + TurbineChatType.ToString("X"));
+            rootNode.Nodes.Add("Unk1 = " + Unk1.ToString("X"));
+            rootNode.Nodes.Add("Unk2 = " + Unk2.ToString("X"));
+            rootNode.Nodes.Add("Unk3 = " + Unk3.ToString("X"));
+            rootNode.Nodes.Add("Unk4 = " + Unk4.ToString("X"));
+            rootNode.Nodes.Add("Unk5 = " + Unk5.ToString("X"));
+            rootNode.Nodes.Add("Unk6 = " + Unk6.ToString("X"));
+            treeView.Nodes.Add(rootNode);
+        }
+    }
 }
--- aclogview/CM_Communication.cs
+++ aclogview/CM_Communication.cs
@@ -13,6 +13,13 @@ public class CM_Communication : MessageProcessor {
 
         PacketOpcode opcode = Util.readOpcode(messageDataReader);
         switch (opcode) {
+            case PacketOpcode.Evt_Communication__Recv_ChatRoomTracker_ID:
+                {
+                    ChatRoomTracker message = ChatRoomTracker.read(messageDataReader);
+                    message.contributeToTreeView(outputTreeView);
+                    break;
+                }
+           
             case PacketOpcode.Evt_Communication__WeenieError_ID: {
                     WeenieError message = WeenieError.read(messageDataReader);
                     message.contributeToTreeView(outputTreeView);
@@ -67,5 +74,55 @@ public class CM_Communication : MessageProcessor {
             rootNode.Nodes.Add("user_data = " + user_data);
             treeView.Nodes.Add(rootNode);
         }
+    }
+}
+
+public class ChatRoomTracker : Message
+{
+    public uint Allegiance;
+    public uint General;
+    public uint Trade;
+    public uint LFG;
+    public uint Roleplay;
+    public uint Olthoi;
+    public uint Unk1;
+    public uint Unk2;
+    public uint Unk3;
+
+    public static ChatRoomTracker read(BinaryReader binaryReader)
+    {
+        ChatRoomTracker newObj = new ChatRoomTracker();
+        newObj.Allegiance = binaryReader.ReadUInt32();
+        newObj.General = binaryReader.ReadUInt32();
+        newObj.Trade = binaryReader.ReadUInt32();
+        newObj.LFG = binaryReader.ReadUInt32();
+        newObj.Roleplay = binaryReader.ReadUInt32();
+        newObj.Olthoi = binaryReader.ReadUInt32();
+        newObj.Unk1 = binaryReader.ReadUInt32();
+        newObj.Unk2 = binaryReader.ReadUInt32();
+        newObj.Unk3 = binaryReader.ReadUInt32();
+
+        return newObj;
+    }
+
+    public override void contributeToTreeView(TreeView treeView)
+    {
+        TreeNode rootNode = new TreeNode(this.GetType().Name);
+        rootNode.Expand();
+        foreach (var prop in this.GetType().GetProperties())
+        {
+            rootNode.Nodes.Add("prop.Name = " + prop.GetValue(this).ToString());
+        }
+            rootNode.Nodes.Add("AllegianceChannelID = " + Allegiance.ToString("X"));
+        rootNode.Nodes.Add("General = " + General.ToString("X"));
+        rootNode.Nodes.Add("Trade = " + Trade.ToString("X"));
+        rootNode.Nodes.Add("LFG = " + LFG.ToString("X"));
+        rootNode.Nodes.Add("Roleplay = " + Roleplay.ToString("X"));
+        rootNode.Nodes.Add("Olthoi = " + Olthoi.ToString("X"));
+        rootNode.Nodes.Add("Unk1 = " + Unk1.ToString("X"));
+        rootNode.Nodes.Add("Unk2 = " + Unk2.ToString("X"));
+        rootNode.Nodes.Add("Unk3 = " + Unk3.ToString("X"));
+
+        treeView.Nodes.Add(rootNode);
     }
 }
